be49573e5cf2945bf4e68b6853f39c9aff4effe5
Juan Quintero <juan.seb.quintero@gmail.com>
2021-09-02 01:08:04 -0500

    fix: adjust navbar component

--- components/HausIcon/index.vue
+++ components/HausIcon/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative inline-block">
     <span :class="iconClass"></span>
-    <span v-if="hasBadge" class="absolute top-0 right-0 | inline-block w-3 h-3 | transform translate-x-1/2 -translate-y-1 | bg-caribbean-green rounded-full"></span>
+    <span v-if="hasBadge" :class="badgeClass"></span>
   </div>
 </template>
 
@@ -14,9 +14,14 @@ export default class HausIcon extends Vue {
   @Prop({ required: true }) readonly iconName!: string;
   @Prop({ default: true }) readonly hasBadge!: boolean;
   @Prop(String) readonly customIconStyle: String | undefined;
+  @Prop({ default: 'transform translate-x-2/3 -translate-y-1.5' }) readonly customTranslate!: string;
 
   get iconClass() : string {
     return `icon-${this.iconName} ${this.customIconStyle}`
+  }
+
+  get badgeClass(): string {
+    return `absolute top-0 right-0 | inline-block w-3 h-3 | bg-caribbean-green rounded-full | ${this.customTranslate}`;
   }
 }
 </script>
